37c89d00ff89127ecf931fba1c883f837cdb9699
Juliux <julioalvar@users.noreply.github.com>
2023-09-19 14:26:32 +0200

    Update deleteAllFilesInFolder.m

--- deleteAllFilesInFolder.m
+++ deleteAllFilesInFolder.m
@@ -1,4 +1,10 @@
 % call the function with the folder path you want to delete
+% ***************************************************************************
+% Created by:   Julio Alvarez
+% Date: 	      AUG-2019 
+% Contact:      julioalvar@gmail.com
+% ***************************************************************************
+
 function deleteAllFilesInFolder(dicom_output_path)
 
 filesInFolder = dir(dicom_output_path);
