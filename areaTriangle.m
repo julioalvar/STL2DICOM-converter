726d91fa52af90988b0d46386e94db86c66fea1c
Juliux <julioalvar@users.noreply.github.com>
2023-09-19 14:26:14 +0200

    Update areaTriangle.m

--- areaTriangle.m
+++ areaTriangle.m
@@ -1,4 +1,10 @@
 % This function returns the Area of the triangle delimited by three points
+% ***************************************************************************
+% Created by:   Julio Alvarez
+% Date: 	      AUG-2019 
+% Contact:      julioalvar@gmail.com
+% ***************************************************************************
+
 function area = areaTriangle(Point1, Point2, Point3)
 
 P1P2 = Point2 - Point1;
